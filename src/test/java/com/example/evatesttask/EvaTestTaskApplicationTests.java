e536e6ff9d0e98832c23de1bd3ae1e854d3e98be
vadimkik2 <vadimkik@ukr.net>
2022-12-14 17:53:40 +0200

    Added maven dependencies and checkstyle plugin.

---
+++ src/test/java/com/example/evatesttask/EvaTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evatesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
